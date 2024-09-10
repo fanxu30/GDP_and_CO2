609ab079c115c483866f551311a1f23ddbb882f6
Fan Xu <fanxu2017@gmail.com>
2024-09-10 14:09:09 -0400

    added analysis file

---
+++ analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn.objects as so
+from matplotlib import style
+
+wdi = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+mort_rate = wdi["Mortality rate, infant (per 1,000 live births)"]
+GDP_per = wdi["GDP per capita (constant 2010 US$)"]
+country_name = wdi["Country Name"]
+
+my_chart = (
+    so.Plot(
+        wdi,
+        x="GDP per capita (constant 2010 US$)",
+        y="Mortality rate, under-5 (per 1,000 live births)",
+    )
+    .add(so.Line(), so.PolyFit(order=2))
+    .add(so.Dot())
+    .label(title="Log GDP and Under-5 Mortality")
+    .theme({**style.library["seaborn-v0_8-whitegrid"]})
+)
+
+my_chart
